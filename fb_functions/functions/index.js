7ae314c37d416465e84c5f707542e22666744c92
Hesaam Farhang <hesaam@2600hz.com>
2017-03-12 23:57:49 -0700

    measure times in cloud function

--- fb_functions/functions/index.js
+++ fb_functions/functions/index.js
@@ -13,6 +13,8 @@ exports.sendMessagetoGroup = functions.database.ref('/group_chats/{groupid}/{mes
     const group_id = event.params.groupid;
     const message_id = event.params.messageid;
 
+    const t0 = _.now();
+
     if (!event.data.val()) {
         return console.log('no payload in message');
     }
@@ -23,6 +25,8 @@ exports.sendMessagetoGroup = functions.database.ref('/group_chats/{groupid}/{mes
 
     // Get the list of members
     return admin.database().ref(`/groups/${group_id}/members`).once('value').then(result => {
+        const t1 = _.now();
+        let t2;
         if (!result.hasChildren()) {
             return console.log(`group ${group_id} has no members`);
         }
@@ -32,6 +36,7 @@ exports.sendMessagetoGroup = functions.database.ref('/group_chats/{groupid}/{mes
         console.log(JSON.stringify(result.val(), null, 2))
 
         _.map(result.val(), (member) => {
+                t2 = _.now();
                 memberIds.push(member.user_id);
                 updates[`/messages/${member.user_id}/${message_id}`] = message;
         })
@@ -41,10 +46,12 @@ exports.sendMessagetoGroup = functions.database.ref('/group_chats/{groupid}/{mes
         }
 
         console.log(`there are ${memberIds.length} memberIds in ${group_id}`);
-
+        const t3 = _.now();
         updates[`/pn_worker/${message_id}`] = message;
         return admin.database().ref().update(updates).then(() => {
+            const t4 = _.now();
             console.log(`[fanout] message ${message_id} delivered at ${_.now()}`);
+            console.log(`elapsed: start=${t0} total=${t4 - t0} members_fetch=${t1 - t0} loop=${t2 - t1} update=${t4 - t3} end=${t4}`);
         });
     })
 });
